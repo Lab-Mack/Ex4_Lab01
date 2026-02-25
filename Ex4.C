d62fb30a9c9ec160f2ee33e7b8ddcf0d28747012
Lab-Mack <10436275@mackenzista.com.br>
2026-02-24 22:22:07 -0300

    Create Ex4.C

---
+++ Ex4.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float largura, comprimento, area;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%f", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%f", &comprimento);
+
+    area = largura * comprimento;
+
+    printf("Area do terreno = %.2f\n", area);
+
+    return 0;
+}
